54e1cec33eb6caa45cd297d274b23ddfb6a175d1
Sitdhibong Laokok <sitdhibong@gmail.com>
2020-12-02 20:11:48 +0700

    Update web structure

--- src/main/java/com/hana/delivery/web/controller/HomePageController.java
+++ src/main/java/com/hana/delivery/web/controller/HomePageController.java
@@ -3,6 +3,7 @@ package com.hana.delivery.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomePageController {
@@ -11,6 +12,7 @@ public class HomePageController {
 	public String index(ModelMap model) {
 		String hello = "Hello from model";
 		model.addAttribute("test", hello);
+		model.addAtribute('name', 'Champ')
 		
 		return "index";
 	}
